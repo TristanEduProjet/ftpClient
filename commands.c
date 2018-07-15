5a31842997dd4e0785a5b6dc9061b4f10a659d9e
Alexandre Felix <alexandrefelix@Alexandres-iMac.local>
2018-07-15 14:51:33 +0200

    added comment

--- commands.c
+++ commands.c
@@ -8,42 +8,49 @@
 #include <unistd.h>
 
 
-
+//liste les fichiers 
 int ls() {
     system("ls>ls.txt");
     return 1;
 }
 
+//change directory
 int cd(char filename[]) {
     return chdir(filename) == 0 ? 1 : 0;
 }
 
-
+//recupere le working directory
 int pwd() {
     system("pwd>pwd.txt");
     return 1;
 }
 
+//supprime le fichier
 int rm(char filename[]) {
     system(strcat("rm", filename));
     return 1;
 }
 
 
-
+//dl un fichier 
 int downl(int sock, char filename[], char buf[], size_t size, int index)
 {
     int fd;
 
+    //creer la commande à envoyer
     strcpy(buf, "downl ");
     strcat(buf, filename);
 
+    //envoie la commande 
     send(sock, buf, 128, 0);
+    //receive la taille à télécharger
     recv(sock, &size, sizeof(int), 0);
 
+    //alloue la memoire puis dl le fichier 
     char* f = malloc(size);
     recv(sock, f, size, 0);
 
+    //ecrit le fichier 
     fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
     write(fd, f, size);
     close(fd);
@@ -51,24 +58,32 @@ int downl(int sock, char filename[], char buf[], size_t size, int index)
     return 1;
 }
 
+//upload un fichier sur le serveur 
 int upld(int sock, char filename[], char buf[], struct stat obj, size_t size)
 {
+    //filedescriptor du fichier en readonly
     int fd = open(filename, O_RDONLY);
     int status;
 
+    //si erreur de lecture on affiche l'erreur associé 
     if(fd == -1)
     {
         printf("%s\n","No such file on the local directory.");
         return 0;
     }
 
+    //on prepare la commande 
     strcpy(buf, "upld ");
     strcat(buf, filename);
+    //on envoie la commande 
     send(sock, buf, 128, 0);
+    //on recupere les infos du fichier 
     stat(filename, &obj);
+    //on envoie la taille du fichier
     size = (size_t) obj.st_size;
     send(sock, &size, sizeof(int), 0);
 
+    //on charge le contenu du fichier en memoire 
     char* content = malloc(size);
     int i = 0;
     FILE*f = fopen(filename,"r");
@@ -77,12 +92,14 @@ int upld(int sock, char filename[], char buf[], struct stat obj, size_t size)
     content[i-1] = '\0';
     fclose(f);
 
+    //on envoie le fichier et on receive le statut retourné par le serveur 
     send(sock, content, size, 0);
     recv(sock, &status, sizeof(int), 0);
 
     return status;
 }
 
+//remote pathname working directory
 char* rpwd(int sock, char buf[])
 {
     strcpy(buf, "rpwd");
@@ -93,19 +110,29 @@ char* rpwd(int sock, char buf[])
     return buf;
 }
 
+//remote ls
 int rls(int sock, char buf[], size_t size)
 {
+    //on prepare et envoie la commande 
     strcpy(buf, "rls");
     send(sock, buf, 100, 0);
+
+    //le serveur renvoie la taille du listing 
     recv(sock, &size, sizeof(unsigned long), 0);
+
+    //recois le listing depuis le serveur 
     char* f = malloc(size);
     recv(sock, f, size, 0);
+
+    //stocke temporairement le listing dans un fichier
+    //ouvert en ecriture, lock exclusive 
     int fd = open("temp.txt", O_CREAT | O_EXCL | O_WRONLY, 0666);
     write(fd, f, size);
     close(fd);
     return 1;
 }
 
+//remote change directory
 int rcd(int sock, char filename[], char buf[])
 {
     int status;
@@ -119,6 +146,7 @@ int rcd(int sock, char filename[], char buf[])
     return status;
 }
 
+//ferme la session ftp
 int quit(int sock, char buf[])
 {
     int status;
--- main.c
+++ main.c
@@ -7,13 +7,17 @@
 
 #include "commands.h"
 
+//loop client
 int loop(int sock) {
+
     struct stat obj;
     size_t size;
     int success;
     char buf[128], filename[96], command[8], *response;
 
     int i = 1;
+    //boucle qui s'occuppe de gerer ce que l'utilisateur rentre et 
+    //executer la fonction correspondante 
     while(1)
     {
         printf("%s\n","Enter a command (ls, cd, pwd, rm, downl, upld, rpwd, rls, rcd, quit): ");
@@ -64,6 +68,7 @@ int loop(int sock) {
             scanf("%s", filename);
             success = rcd(sock, filename, buf);
             printf("%s\n", success ? "Directory changed successfully !" : "Error when trying to change directory");
+        //c'est cette commande qui sors de la boucle apres avoir fermé la connection
         } else if(!strcmp(command, "quit")) {
             success = quit(sock, buf);
             if(success) {
@@ -83,27 +88,33 @@ int main(int argc,char *argv[])
     char buf[128];
     int sock, k;
 
+    //on creer un socket en TCP, IPV4
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == -1) {
         printf("socket creation failed");
         return 1;
     }
 
+    //configuration adresse serveur
     server.sin_family = AF_INET;
     server.sin_port = htons(1977);
     server.sin_addr.s_addr = 0;
 
+    //connection au serveur 
     k = connect(sock, (struct sockaddr*)&server, sizeof(server));
     if(k == -1) {
         printf("Connection error");
         return 1;
     }
 
+    //envoie "ping" de présence
     send(sock, "BONJ", 128, 0);
     recv(sock, &buf, 128, 0);
     printf("%s\n", buf);
 
+
     if(strcmp(buf, "WHO") == 0) {
+        //login
         scanf("%s", buf);
         send(sock, buf, 128, 0);
         recv(sock, &buf, 128, 0);
@@ -111,11 +122,14 @@ int main(int argc,char *argv[])
 
         if(strcmp(buf, "PASSWD") == 0) {
             while (1) {
+                //mot de passe 
                 scanf("%s", buf);
                 send(sock, buf, 128, 0);
                 recv(sock, &buf, 128, 0);
                 printf("%s\n", buf);
 
+                // on lance la boucle de command ou on quitte 
+                //si le password est incorrect
                 if(strcmp(buf, "WELC") == 0) {
                     return loop(sock);
                 } else if(strcmp(buf, "BYE") == 0) {
